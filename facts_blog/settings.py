b2a1f3760e19547983ed778f580d0329bd78e60b
RaktimParasar <raktimparasar96@gmail.com>
2020-01-08 17:44:17 +0530

    Added custom 404 and 500 error pages.

--- facts_blog/settings.py
+++ facts_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k^%e$z$8codtf6e%)wb*wzywhyxh((nvtt*s$tmy^$ekf#99_+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'facts_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
