4830c9c65c97e6c429a26c01cb43a3bc8d5b5eff
RaktimParasar <raktimparasar96@gmail.com>
2020-01-08 10:36:58 +0530

    Set DEBUG based on enviroment variables

--- facts_blog/settings.py
+++ facts_blog/settings.py
@@ -131,3 +131,8 @@ LOGIN_URL = 'users:login'
 #Heroku settings
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'True':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'False':
+    DEBUG = False
